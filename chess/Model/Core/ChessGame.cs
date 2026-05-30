6c531ef957a8d892a4731659e830634fa66d3fc0
KiraFlux <pret2titio1n@gmail.com>
2026-05-30 15:32:23 +0300

    rook_piece_t

--- chess/Model/Core/ChessGame.cs
+++ chess/Model/Core/ChessGame.cs
@@ -162,22 +162,22 @@ public partial class ChessGame {
             board.piece_place(new pawn_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(1, column)));
         }
 
-        board.piece_place(new Rook(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 0)));
+        board.piece_place(new rook_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 0)));
         board.piece_place(new knight_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 1)));
         board.piece_place(new Bishop(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 2)));
         board.piece_place(new queen_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 3)));
         board.piece_place(new king_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 4)));
         board.piece_place(new Bishop(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 5)));
         board.piece_place(new knight_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 6)));
-        board.piece_place(new Rook(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 7)));
+        board.piece_place(new rook_piece_t(piece_color_t.PIECE_COLOR_WHITE, new position_t(7, 7)));
 
-        board.piece_place(new Rook(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 0)));
+        board.piece_place(new rook_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 0)));
         board.piece_place(new knight_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 1)));
         board.piece_place(new Bishop(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 2)));
         board.piece_place(new queen_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 3)));
         board.piece_place(new king_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 4)));
         board.piece_place(new Bishop(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 5)));
         board.piece_place(new knight_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 6)));
-        board.piece_place(new Rook(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 7)));
+        board.piece_place(new rook_piece_t(piece_color_t.PIECE_COLOR_BLACK, new position_t(0, 7)));
     }
 }
